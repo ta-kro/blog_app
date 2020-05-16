3e9b12d984a23986c979ab34ac9ba05bbd7647e9
ta-kro <takuroizumi1109@gmail.com>
2020-05-16 07:45:38 +0000

    modified controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,17 +14,43 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "編集しました"
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "削除しました"
+    redirect_to posts_path
+  end
+
+  
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
