9e9bcd375b00e6ab26c5fb25a6f51f4093b1b39e
ta-kro <takuroizumi1109@gmail.com>
2020-05-16 22:36:22 +0000

    modified users' controller and index and new views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all.order(created_at: :desc)
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params_create)
+    if @user.save
+      session[:user_id]  = @user.id
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -27,5 +37,14 @@ class UsersController < ApplicationController
   end
 
   def logout
+  end
+
+  private
+  def user_params_create
+    params.require(:user).permit(:name, :email, :password, :password_confirm)
+  end
+
+  def user_params_edit
+    params.require(:user).permit(:name, :email)
   end
 end
