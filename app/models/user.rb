958d28bbecfc41437691d04edb6b58dc9629b2cf
ta-kro <takuroizumi1109@gmail.com>
2020-05-16 00:56:19 +0000

    modified models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :posts, dependent: :destroy
   before_save { email.downcase! }
 
   validates :name, presence: true, length: {maximum: 50}
