f30dd6f9aff63d65c33cfae9b609887249ad8e0e
ta-kro <takuroizumi1109@gmail.com>
2020-05-16 00:05:14 +0000

    set controllers, models, views and layouts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  validates :content, presence: true, length: {maximum: 140, message: "は140字以内で入力してください"}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :posts, dependent: :destroy
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: {maximum: 50}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: {minimum: 6}, on: :create
+  #「on: :create」でcreateアクションの時だけバリデーションが実行される
+
 end
