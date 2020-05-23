f99d1213c813b20a3e58a729e75aee15a6c6cd89
ta-kro <takuroizumi1109@gmail.com>
2020-05-23 23:16:53 +0000

    modified post form and error messages and more

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  
   def home
   end
 
@@ -18,7 +21,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    # @post = Post.new(post_params)
+    @user = User.find(@current_user.id)
+    @post = @user.posts.build(post_params)
     if @post.save
       flash[:notice] = "投稿しました"
       redirect_to posts_path
@@ -49,6 +54,14 @@ class PostsController < ApplicationController
   end
 
 
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
 
   private
   def post_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    # binding.pry
     if @user.save
       session[:user_id]  = @user.id
       flash[:notice] = "登録が完了しました"
@@ -84,6 +85,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirm)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
