7929b394f934c3e05e3ecfa372028e8dd4914707
ta-kro <takuroizumi1109@gmail.com>
2020-06-14 08:13:54 +0000

    arranged CSS

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :users
   get '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
